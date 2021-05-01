31a1483df15933822172c88893e15752a9bd849a
Andrew Adare <andrewadare@gmail.com>
2021-05-01 16:01:27 -0600

    Add structs for actuator state and populate in a sensor sampling task. Replace main loop with FreeRTOS timer callback and signaling logic.

--- src/main.c
+++ src/main.c
@@ -1,3 +1,5 @@
+// TODO: use a mutex semaphore for UART traffic
+
 // std lib
 #include <stdio.h>
 
@@ -8,7 +10,9 @@
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/queue.h"
+#include "freertos/semphr.h"
 #include "freertos/task.h"
+#include "freertos/timers.h"
 
 // local
 #include "chip_introspection.h"
@@ -19,17 +23,64 @@
 uint8_t as5600_registers[2] = {0x0d, 0x0c};  // for low and high bytes
 
 // Pin assignments on ESP32 dev board
-static gpio_num_t onboard_led_pin = 2;
-static gpio_num_t motor_pwm_pin = 4;
-static gpio_num_t limit_switch_pin = 16;
-static gpio_num_t as5600_sda = 18;
-static gpio_num_t as5600_scl = 19;
+static const gpio_num_t onboard_led_pin = 2;
+static const gpio_num_t motor_pwm_pin = 4;
+static const gpio_num_t limit_switch_pin = 16;
+static const gpio_num_t as5600_sda = 18;
+static const gpio_num_t as5600_scl = 19;
+
+// Sets the target sample and update period of the overall system
+static const TickType_t update_period = pdMS_TO_TICKS(10);
 
 static xQueueHandle limit_switch_queue;
+
+xSemaphoreHandle master_tick_signal;
 
 static void IRAM_ATTR limit_switch_isr(void* args) {
   int pin_number = (int)args;
   xQueueSendFromISR(limit_switch_queue, &pin_number, NULL);
+}
+
+typedef enum { MOVING_DOWN, MOVING_UP, STATIONARY } actuator_direction_t;
+
+typedef struct {
+  int position;         // 0-4095, datum arb. but fixed
+  float rotation_rate;  // signed ang. speed in 12-bit units per timestep
+  int rotations;        // signed rotation count
+  float radius;         // [cm] Half pitch diam. For rot. to lin. conversion
+} pulley_t;
+
+typedef struct {
+  pulley_t pulley;
+  float position;                  // cm above limit switch
+  float speed;                     // cm/s, positive upward
+  actuator_direction_t direction;  // up/down/stationary
+} actuator_t;
+
+void master_timer_callback(TimerHandle_t timer) {
+  xSemaphoreGive(master_tick_signal);
+}
+
+void sample_as5600_task(void* params) {
+  esp_err_t ret = ESP_FAIL;
+  uint16_t position = 0;  // angle encoded as a 12 bit value
+
+  actuator_t* actuator = (actuator_t*)params;
+
+  while (true) {
+    // TODO: time out after 2 master ticks
+    xSemaphoreTake(master_tick_signal, portMAX_DELAY);
+
+    ret = i2c_read_two_bytes(AS5600_ADDR, as5600_registers, &position);
+    if (ret == ESP_ERR_TIMEOUT) {
+      ESP_LOGE("AS5600", "I2C timeout");
+    } else if (ret == ESP_OK) {
+      actuator->pulley.position = position;
+      ESP_LOGI("AS5600", "%d", actuator->pulley.position);
+    }
+
+    vTaskDelay(pdMS_TO_TICKS(1));
+  }
 }
 
 void limit_switch_task(void* params) {
@@ -43,14 +94,17 @@ void limit_switch_task(void* params) {
 
       // Interrupt handler logic
       if (gpio_get_level(pin_number) == 1) {
-        ESP_LOGW("STATUS", "Limit switch triggered (%d). GPIO %d = %d", count++,
-                 pin_number, gpio_get_level(pin_number));
+        count++;
+        ESP_LOGW("LIMIT_SWITCH", "Triggered %d %s since start. GPIO %d = %d",
+                 count, count == 1 ? "time" : "times", pin_number,
+                 gpio_get_level(pin_number));
         gpio_set_level(onboard_led_pin, 1);
       }
-      vTaskDelay(pdMS_TO_TICKS(100));
+      vTaskDelay(pdMS_TO_TICKS(100));  // debounce delay
 
-      // Reenable
+      // Reenable interrupt after debounce period
       gpio_isr_handler_add(pin_number, limit_switch_isr, (void*)pin_number);
+
       gpio_set_level(onboard_led_pin, 0);
     }
   }
@@ -72,7 +126,6 @@ void configure_limit_switch_pin() {
   gpio_config(&conf);
 
   limit_switch_queue = xQueueCreate(1, sizeof(int));
-  xTaskCreate(limit_switch_task, "limit_switch_task", 2048, NULL, 1, NULL);
   gpio_install_isr_service(0);
   gpio_isr_handler_add(limit_switch_pin, limit_switch_isr,
                        (void*)limit_switch_pin);
@@ -101,6 +154,9 @@ void configure_motor_pwm() {
 }
 
 void app_main() {
+  actuator_t actuator;
+  master_tick_signal = xSemaphoreCreateBinary();
+
   configure_onboard_led();
   configure_limit_switch_pin();
   configure_motor_pwm();
@@ -110,24 +166,11 @@ void app_main() {
   print_device_info();
   print_memory();
 
-  esp_err_t ret = ESP_FAIL;
-  uint16_t angle_12bit = 0;
-  int led_fade_period = 1000;
-  while (true) {
-    // TODO move to a dedicated task
-    ret = i2c_read_two_bytes(AS5600_ADDR, as5600_registers, &angle_12bit);
-    if (ret == ESP_ERR_TIMEOUT) {
-      ESP_LOGE("AS5600", "I2C timeout");
-    } else if (ret == ESP_OK) {
-      ESP_LOGI("AS5600", "%d", angle_12bit);
-    }
+  xTaskCreate(&limit_switch_task, "limit_switch_task", 2048, NULL, 5, NULL);
+  xTaskCreate(&sample_as5600_task, "sample_as5600_task", 2048, &actuator, 2,
+              NULL);
 
-    // Temporary - will probably use MCPWM instead of LEDC
-    ledc_set_fade_time_and_start(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, 0,
-                                 led_fade_period / 2, LEDC_FADE_WAIT_DONE);
-    ledc_set_fade_time_and_start(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, 1024,
-                                 led_fade_period / 2, LEDC_FADE_WAIT_DONE);
-
-    vTaskDelay(pdMS_TO_TICKS(100));
-  }
+  TimerHandle_t master_timer = xTimerCreate("master_timer", update_period, true,
+                                            NULL, &master_timer_callback);
+  xTimerStart(master_timer, 0);
 }
