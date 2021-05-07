fe33a18e2d0550f0d10670538565568f120c6603
Andrew Adare <andrewadare@gmail.com>
2021-05-06 20:22:42 -0600

    Remove pulley_t

--- src/actuator.h
+++ src/actuator.h
@@ -10,18 +10,12 @@ typedef enum {
 } actuator_direction_t;
 
 typedef struct {
-  int position;         // 0-4095
-  int datum;            // start/rollover value for rotation counting
-  float rotation_rate;  // signed ang. speed in 12-bit units per timestep
-  int rotations;        // signed rotation count
-  float radius;         // [cm] Half pitch diam. For rot. to lin. conversion
-} pulley_t;
-
-typedef struct {
-  pulley_t pulley;
   float position;                  // cm above limit switch
   float speed;                     // cm/s, positive upward
   actuator_direction_t direction;  // linear up/down direction
+  float pulley_radius;             // [cm] Half pitch diam.
+  int encoder_units;               // position in encoder ticks
+  int datum;                       // encoder ticks at low point
   bool homed;                      // limit switch contacted
 } actuator_t;
 
